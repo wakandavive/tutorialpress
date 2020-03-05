d5b4b4f8951be3d06703985257f1956d0d77bb61
Kauã Jotta <50594124+wakandavive@users.noreply.github.com>
2020-03-04 21:23:34 -0300

    Aula curso 2

---
+++ curso-djngo/tutorialpress/core/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from rest_framework import routers, serializers, viewsets
+
+from tutorialpress.core.models import Publicacao
+
+
+class PublicacaoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Publicacao
+        fields = ["id", "titulo", "conteudo", "is_publicada"]
+
+class PublicacaoViewSet(viewsets.ModelViewSet):
+    queryset = Publicacao.objects.all()
+    serializer_class = PublicacaoSerializer
+
+router = routers.SimpleRouter()
+router.register("publicacoes", PublicacaoViewSet)
+
