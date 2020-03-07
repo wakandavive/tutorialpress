087f7c012ad8cf4af9eaf71ed7ae050fcd8ed5b8
Kauã Jotta <50594124+wakandavive@users.noreply.github.com>
2020-03-07 16:37:33 -0300

    Add files via upload

---
+++ tests/integration/test_categoria.py
@@ -0,0 +1,37 @@
+import pytest
+from django.urls import reverse
+from rest_framework import status
+
+from tutorialpress.core.models import Categoria
+
+
+@pytest.mark.django_db
+def test_create_valid(api_client):
+    # DADO dados de categoria válidos.
+    dados_categoria = {"nome": "Categoria A"}
+
+    # QUANDO a API é chamada para criar uma categoria.
+    resp = api_client.post(reverse("categoria-list"), dados_categoria)
+
+    # ENTÃO a reposta de sucesso deve conter os dados da categoria
+    assert resp.status_code == status.HTTP_201_CREATED
+    assert isinstance(resp.data["id"], int)
+
+    # E ENTÃO a categoria deve existir no banco.
+    assert Categoria.objects.get(pk=resp.data["id"]).nome == dados_categoria["nome"]
+
+
+@pytest.mark.django_db
+def test_create_invalid(api_client):
+    # DADO dados de categoria inválidos.
+    dados_categoria = {"campo_invalido": "123"}
+
+    # QUANDO a API é chamada para criar uma categoria.
+    resp = api_client.post(reverse("categoria-list"), dados_categoria)
+
+    # ENTÃO a reposta de falha deve conter os erros de cada campo.
+    assert resp.status_code == status.HTTP_400_BAD_REQUEST
+    assert len(resp.data["nome"]) != 0
+
+    # E ENTÃO a categoria não deve existir no banco.
+    assert Categoria.objects.count() == 0
